548dc3eeae7b3fd09ac2246c99b34dbeaf44b9b8
gjy0605 <gjy0605@gmail.com>
2023-04-06 01:34:07 +0900

    feat: 체크리스트 컴포넌트 생성

---
+++ src/components/CheckList.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+
+const CheckList = () => {
+
+  return (
+    <div className="App">
+      <h2>124124</h2>
+    </div>
+  );
+};
+
+export default CheckList;
